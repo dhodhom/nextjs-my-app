48ca6e8f54df719ef9ca0b26d4da37a6b68ef3df
unknown <dhodhom@gmail.com>
2024-06-28 09:42:31 +0700

    5

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,5 +6,5 @@ import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return <div>asdfasdf</div>;
+  return <div>index</div>;
 }
